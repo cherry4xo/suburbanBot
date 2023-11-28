d3d4548638553994f8e38987fbac293828631321
cherry4xo <1jlaketuk1@gmail.com>
2023-11-28 12:04:15 +0300

    v0.6.0.0 fixed bugs with states, requests and so on

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 import asyncio
 import aiohttp
 
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from telebot.async_telebot import AsyncTeleBot
 from telebot.asyncio_storage import StateMemoryStorage
 from telebot.asyncio_filters import StateFilter
@@ -30,29 +30,28 @@ class States(StatesGroup):
     search_start_station = State()
     search_finish_station = State()
 
-
 async def get_schedule_request(session, to_user: User, start_station_id: str, finish_station_id: str):
     url = f"https://api.rasp.yandex.net/v3.0/search/?apikey={settings.YANDEX_API_TOKEN}&format=json&from={start_station_id}&to={finish_station_id}&lang=ru_RU&page=1&date={date.isoformat(date.today())}"
     try:
-        async with session.post(url=url) as response:
-            return response
+        async with session.get(url=url) as response:
+            return await response.json()
     except BaseException:
-        bot.send_message(to_user.tg_id, text=texts.cannot_get_schedule)
+        bot.send_message(to_user.tg_id, text=texts.cannot_get_schedule, reply_markup=menu.go_to_main_menu_only())
 
 
 async def get_schedule_task(to_user: User, start_station_id: str, finish_station_id: str):
-    async with aiohttp.ClientSession() as session:
+    async with aiohttp.ClientSession(headers={"Accept": "application/json"}) as session:
         task = get_schedule_request(session, to_user, start_station_id, finish_station_id)
         return await asyncio.gather(task)
     
 
 def schedule_filter(trip: dict):
-    return datetime.fromisoformat(trip["departure"]) > datetime.utcnow()
+    return datetime.fromisoformat(trip["departure"]).strftime("%Y-%m-%d %H:%M:%S") > (datetime.utcnow() + timedelta(hours=3)).strftime("%Y-%m-%d %H:%M:%S")
 
 
 def get_normalized_schedule_response(schedule: dict) -> dict:
     norm_schedule = []
-    for train in filter(schedule_filter, schedule["segments"])[:3]:
+    for train in list(filter(schedule_filter, schedule["segments"]))[:3]:
         trip = {"number": train["thread"]["number"],
                 "title": train["thread"]["title"],
                 "train_subtype": train["thread"]["transport_subtype"]["title"],
@@ -106,13 +105,14 @@ async def search_start_station(msg: telebot.types.Message):
                                         "id": station["id"], 
                                         "region_title": region_title})
 
+    await bot.delete_state(msg.from_user.id, msg.chat.id)
     await bot.send_message(msg.chat.id,
                            text=texts.choose_start_station,
                            reply_markup=menu.searched_start_stations(search_list))
 
 
 @bot.message_handler(state=States.search_finish_station)
-async def search_finish_station(msg: telebot.types.Message) -> list:
+async def search_finish_station(msg: telebot.types.Message):
     search_list = []  
     with open("./app/data/data_russia_trains.json", "r") as f:
         data = json.load(f)
@@ -126,8 +126,6 @@ async def search_finish_station(msg: telebot.types.Message) -> list:
     await bot.send_message(msg.chat.id,
                            text=texts.choose_finish_station,
                            reply_markup=menu.searched_finish_stations(search_list))
-
-    await bot.delete_state(user_id=msg.from_user.id, chat_id=msg.chat.id)
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("get_favorites_"))
@@ -163,8 +161,8 @@ async def new_route_callback_handler(call: telebot.types.CallbackQuery):
     if not user:
         await user_does_not_exist_message(call.from_user.id)
 
-    await bot.send_message(call.message.chat.id, text=texts.search_start_station, reply_markup=menu.go_to_main_menu_only())
     await bot.set_state(user.tg_id, States.search_start_station, call.message.chat.id)
+    await bot.send_message(call.message.chat.id, text=texts.search_start_station, reply_markup=menu.go_to_main_menu_only())
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("startstation_"))
@@ -174,33 +172,33 @@ async def pick_finish_station_callback_handler(call: telebot.types.CallbackQuery
     if not user:
         await user_does_not_exist_message(call.from_user.id)
 
-    data = call.data.split('_')[1]
-    start_station_title = data[1][1:-1]
-    start_station_id = data[2]
+    data = call.data.split('_')[1:]
+    start_station_title = data[0][1:-1]
+    start_station_id = data[1]
 
-    temp_station_search.update({call.from_user.id: {"title": start_station_title, 
-                                                    "id": start_station_id}})
-    
-    await bot.set_state(call.message.from_user.id, States.search_finish_station, call.message.chat.id)
+    temp_station_search.update({user.tg_id: {"title": start_station_title, 
+                                             "id": start_station_id}})
+
+    await bot.set_state(user.tg_id, States.search_finish_station, call.message.chat.id)
     await bot.send_message(call.message.chat.id, text=texts.search_finish_station, reply_markup=menu.go_to_main_menu_only())
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("finishstation_"))
 async def make_route_callback_handler(call: telebot.types.CallbackQuery):
-    user = await User.get_by_tg_id(call.from_user.tg_id)
+    user = await User.get_by_tg_id(call.from_user.id)
 
     if not user:
         await user_does_not_exist_message(call.from_user.id)
 
-    data = call.data.split('_')[1]
-    finish_station_title = data[1][1:-1]
-    finish_station_id = data[2]
+    data = call.data.split('_')[1:]
+    finish_station_title = data[0][1:-1]
+    finish_station_id = data[1]
 
-    start_station_data = temp_station_search.pop(call.from_user.id)
+    start_station_data = temp_station_search.pop(user.tg_id)
 
-    schedule = get_schedule_task(user, start_station_data["title"], finish_station_id)
+    schedule = await get_schedule_task(user, start_station_data["id"], finish_station_id)
 
-    normalized_schedule = get_normalized_schedule_response(schedule)
+    normalized_schedule = get_normalized_schedule_response(schedule[0])
 
     await bot.send_message(call.message.chat.id, 
                            text=texts.get_schedule(normalized_schedule), 
